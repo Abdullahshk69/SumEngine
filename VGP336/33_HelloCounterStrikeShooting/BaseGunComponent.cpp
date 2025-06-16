75e8ffd7f7a473f429257b6e929560349c76902f
Abdullah Sheikh <abdullahsheikh1998@gmail.com>
2025-06-16 14:30:15 -0700

    Implemented base gun component and shooting logic

---
+++ VGP336/33_HelloCounterStrikeShooting/BaseGunComponent.cpp
@@ -0,0 +1,95 @@
+#include "BaseGunComponent.h"
+#include "CS_FPSControllerComponent.h"
+
+using namespace SumEngine;
+using namespace SumEngine::Input;
+using namespace SumEngine::Math;
+using namespace SumEngine::Physics;
+
+void BaseGunComponent::Initialize()
+{
+    mSoundBankComponent = GetOwner().GetComponent<SoundBankComponent>();
+}
+
+void BaseGunComponent::Terminate()
+{
+    mAgent = nullptr;
+    mSoundBankComponent = nullptr;
+}
+
+void BaseGunComponent::Update(float deltaTime)
+{
+    if (mCurrentFireBuffer > 0.0f)
+    {
+        mCurrentFireBuffer -= deltaTime;
+    }
+
+    if (mReloadTimerBuffer > 0.0f)
+    {
+        mReloadTimerBuffer -= deltaTime;
+
+        if (mReloadTimerBuffer <= 0.0f)
+        {
+            ReloadFinish();
+        }
+    }
+}
+
+void BaseGunComponent::Shoot()
+{
+    if (mCurrentFireBuffer > 0.0f || mIsReloading || !mIsEquipped)
+    {
+        return;
+    }
+
+    Vector3 aim = mAgent->GetForwardVector();
+    Raycast raycast;
+    bool hit = raycast.Cast(mAgent->GetPosition(), mAgent->GetForwardVector(), 100.0f);
+    
+    if (hit)
+    {
+        RaycastHit* raycastHit = raycast.GetRaycastResult();
+        GameObject* hitObject = raycastHit->gameObject;
+
+        RigidBodyComponent* rb = hitObject->GetComponent<RigidBodyComponent>();
+        if (rb)
+        {
+            rb->AddForce(mAgent->GetForwardVector() * mForce);
+        }
+    }
+    PlayShootingSoundEffect();
+    mCurrentFireBuffer = mFireRate;
+    if (--mCurrentAmmo == 0)
+    {
+        ReloadStart();
+    }
+}
+
+void BaseGunComponent::ReloadStart()
+{
+    mReloadTimerBuffer = mReloadSpeed;
+    mIsReloading = true;
+    PlayReloadingSoundEffect();
+}
+
+void BaseGunComponent::ReloadFinish()
+{
+    mCurrentAmmo = mAmmo;
+    mIsReloading = false;
+}
+
+void BaseGunComponent::Pickup(CS_FPSControllerComponent* agent)
+{
+    mAgent = agent;
+    mIsEquipped = true;
+}
+
+void BaseGunComponent::Drop()
+{
+    mIsEquipped = false;
+}
+
+void BaseGunComponent::Switch()
+{
+    mIsEquipped = false;
+}
--- VGP336/33_HelloCounterStrikeShooting/BaseGunComponent.h
+++ VGP336/33_HelloCounterStrikeShooting/BaseGunComponent.h
@@ -1,1 +1,36 @@
 #pragma once
+
+#include "CustomTypeIds.h"
+
+class CS_FPSControllerComponent;
+class BaseGunComponent : public SumEngine::Component
+{
+public:
+    SET_TYPE_ID(CustomComponentId::BaseGun);
+
+    void Initialize() override;
+    void Terminate() override;
+    void Update(float deltaTime) override;
+
+    virtual void Shoot();
+    virtual void ReloadStart();
+    virtual void ReloadFinish();
+    virtual void Pickup(CS_FPSControllerComponent* agent);
+    virtual void Drop();
+    virtual void Switch();
+    virtual void PlayShootingSoundEffect() {}
+    virtual void PlayReloadingSoundEffect() { mSoundBankComponent->Play("Reloading"); }
+
+protected:
+    int mAmmo = 5;
+    int mCurrentAmmo = 5;
+    float mFireRate = 1.0f;
+    float mCurrentFireBuffer = 0.0f;
+    float mForce = 1.0f;
+    float mReloadSpeed = 2.0f;
+    float mReloadTimerBuffer = 0.0f;
+    bool mIsReloading = false;
+    bool mIsEquipped = false;
+    CS_FPSControllerComponent* mAgent = nullptr;
+    SumEngine::SoundBankComponent* mSoundBankComponent = nullptr;
+};
